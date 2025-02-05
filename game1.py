f70b977493b37201c716b49fea4e7798ee4969ce
Dineth Sanjuna <dinethsanju519@gmail.com>
2025-02-05 17:37:05 +0530

    display score, lives, level, game over message and final score

--- game1.py
+++ game1.py
@@ -6,12 +6,19 @@ import random
 WIDTH = 1200
 HEIGHT = 800
 
+# Set up initial values
+i = 1
+lives = 2
+score = 0
+level = 1
+speed = 5 * i
+
 # Load the background images
 background_1 = Actor('mm.png', (600, 400))
 background_2 = Actor('mm.png', (1800, 400))
 
 # Load image of the aircraft
-aircraft = Actor('aircraft.png', (200, 400))
+aircraft = Actor('aircraft.png', (100, 400))
 
 # Load image of the obstacle
 meteor = Actor('ulka2.png', (1200, 400))
@@ -19,13 +26,31 @@ meteor = Actor('ulka2.png', (1200, 400))
 # Define the draw function
 def draw():
     screen.clear()
-    background_1.draw()     # Draw the background_1 image
-    background_2.draw()     # Draw the background_2 iamge
-    aircraft.draw()         # Draw the aircraft image
-    meteor.draw()           # Draw the meteor image
+    if lives > 0:
+        # Draw the images
+        background_1.draw()    
+        background_2.draw()    
+        aircraft.draw()       
+        meteor.draw()           
+        
+        # Display the score, lives and level
+        screen.draw.text(f"Score: {score}", color="White", topleft=(10, 20), fontsize=40)
+        screen.draw.text(f"Lives: {lives}", color="White", topleft=(10, 50), fontsize=40)
+        screen.draw.text(f"LEVEL: {level}", (500, 20), color="White", fontsize=100)
     
+    elif lives == 0:
+       # Display the game over message and final score
+        screen.draw.text("GAME OVER", color="Red", center=(600, 400), fontsize=200)
+        screen.draw.text(f"Final Score: {score}", color="Yellow", center=(600, 700), fontsize=70)
+    
+        
 # Define the update function   
 def update():
+    global lives, score, level, speed
+    
+    # Stopping the game when game is over
+    if lives == 0:
+        return
     
     # Move the background_1 image to the left
     background_1.x -= 5
@@ -45,6 +70,7 @@ def update():
     if meteor.right < 1:             
         meteor.left = 1200
         meteor.y = random.randint(150, 650)     # Randomize the meteor position of y-axis
+        score += 1      # Increase the score by 1
         
     # Checkibg collision between the aircraft and the meteor
     if meteor.colliderect(aircraft):
@@ -52,7 +78,8 @@ def update():
         # Reset the meteor position
         meteor.x = 1200
         meteor.y = random.randint(150, 650)     # Randomize the meteor position of y-axis
-    
+        lives -= 1      # Reduce the lives by 1
+        
     # Move the aircraft up and down    
     if keyboard.down:
         if aircraft.y < 650:    # Limit bottom movement
